be96f8545f97b29d07e0cd043dc127661a4c89b9
eric.cpu800 <ericpedreros160@gmail.com>
2023-12-07 16:45:16 -0300

    Verificador_Rut

--- conexion.php
+++ conexion.php
@@ -17,4 +17,25 @@
         return $cnn;
     }
 
+    function verificar($caja_rut) {
+        $cont = 2;
+        $suma = 0;
+        $largo = strlen($caja_rut);
+        for ($i = $largo; $i > 0; $i--) {
+            $suma = $suma + (substr($caja_rut, $i - 1, 1) * $cont);
+            $cont = $cont + 1;
+            if ($cont == 8) {
+                $cont = 2;
+            }
+        }
+        $Digito = 11 - ($suma % 11);
+        if ($Digito == 10) {
+            $Digito = 'k';
+        }
+        if ($Digito == 11) {
+            $Digito = "0";
+        }
+        return $Digito;
+    }
+    
 ?>
