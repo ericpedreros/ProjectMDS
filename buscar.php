192a1d3e76af20d2acf7e288d5fa2defa6b6a81c
eric.cpu800 <ericpedreros160@gmail.com>
2023-12-14 14:12:57 -0300

    Creacion y arreglo de CRUD Read

---
+++ buscar.php
@@ -0,0 +1,19 @@
+<html>
+    <head><title>admin</title></head>
+    <body bgcolor = "#FFFFFF">
+        <center>
+
+            || <a href = "buscarxtodo.php">Mostrar a Todos</a> ||
+
+            || <a href = "buscarxrut.php">Buscar x Rut</a> ||
+
+            || <a href = "buscarxid.php">Buscar x Id</a> ||
+                    
+            <br></br>
+            
+            <a href = "admin.php">Volver</a>
+
+        </center>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        
-    </head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title> 
+        <link rel="stylesheet" href="./style.css">
+
+    </head>   
     <body>
         <center>
             <form class="form" method="POST" action="">
