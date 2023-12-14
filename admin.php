9fe9cf88bf05a67200b23c70b11e7e5fbed96b44
eric.cpu800 <ericpedreros160@gmail.com>
2023-12-14 15:36:55 -0300

    SessionStart

--- admin.php
+++ admin.php
@@ -1,15 +1,30 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Inicio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel='stylesheet' href="./style.css">
+    <script>
+        function redirectToPage(pageName) {
+            window.location.href = pageName;
+        }
+    </script>
 </head>
-<body bgcolor="#FFFFFF">
-    <center>
-        || <a href="agregar.php">AGREGAR</a> ||
-        || <a href="eliminar.php">ELIMINAR</a> ||
-        || <a href="modificar.php">MODIFICAR</a> ||
-        || <a href="buscar.php">BUSCAR</a> ||
-    
-    </center>
+<body>
+ 
+    <div class="input-field button">
+        <input type="button" onclick="redirectToPage('agregar.php')" value="AGREGAR" />
+
+        <input type="button" onclick="redirectToPage('eliminar.php')" value="ELIMINAR" />
+                            
+        <input type="button" onclick="redirectToPage('modificar.php')" value="MODIFICAR" />
+
+        <input type="button" onclick="redirectToPage('buscar.php')" value="BUSCAR" />
+    </div>
+     
 </body>
 </html>
--- buscar.php
+++ buscar.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html>
     <head><title>admin</title></head>
     <body bgcolor = "#FFFFFF">
