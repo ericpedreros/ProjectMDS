6424234d87ca5dcb2a5f6e028dbac629ef60e77a
eric.cpu800 <ericpedreros160@gmail.com>
2023-12-14 10:13:50 -0300

    Cambiando para agregar inicio sesion

---
+++ admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio</title>
+</head>
+<body bgcolor="#FFFFFF">
+    <center>
+        || <a href="agregar.php">AGREGAR</a> ||
+        || <a href="eliminar.php">ELIMINAR</a> ||
+        || <a href="modificar.php">MODIFICAR</a> ||
+        || <a href="buscar.php">BUSCAR</a> ||
+    
+    </center>
+</body>
+</html>
